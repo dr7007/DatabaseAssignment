f5291596df4f07341dd57934f6f8ba18a00753a5
DESKTOP-P1OUMPK\admin <dndb0413@naver.com>
2024-11-19 17:46:01 +0900

    UI 수정

--- Assets/DatabaseAssignment/Scripts/DBLogin.cs
+++ Assets/DatabaseAssignment/Scripts/DBLogin.cs
@@ -1,16 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class DBLogin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject Login_Access;
+    public GameObject Login_Fail;
 
-    // Update is called once per frame
-    void Update()
+    public TMP_InputField ID_Input;
+    public TMP_InputField PW_Input;
+    public Button Sign_up_BT;
+
+    // 임의 변수
+    private string user = "D";
+    private string password = "1";
+
+    public void LoginButtonClick()
     {
-        
+        if(ID_Input.text == user && PW_Input.text == password)
+        {
+            Debug.Log("로그인 성공");
+            Login_Access.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("로그인 실패");
+            Login_Fail.SetActive(true);
+        }
+    }
+    public void OnCheck()
+    {
+        Debug.Log("확인");
+        transform.parent.gameObject.SetActive(false);
     }
 }
